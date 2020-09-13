00d7dd20ab836bb0afb15a2265462535a5271afd
justinasfour04 <justin.asfour0@gmail.com>
2020-09-13 02:59:07 -0400

    Cleaned up build folder after forgetting

--- build/tile_type.js
+++
@@ -1,6 +0,0 @@
-var TileType;
-(function (TileType) {
-    TileType[TileType["Air"] = 0] = "Air";
-    TileType[TileType["Solid"] = 1] = "Solid";
-})(TileType || (TileType = {}));
-export default TileType;
